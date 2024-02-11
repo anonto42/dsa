aa80ead7f97b6724fe037db9e35090d5dd843b0d
Sohidul Islam Ananto <anonto1080@gmail.com>
2024-02-12 00:27:10 +0600

    add

--- LL.java
+++ LL.java
@@ -24,7 +24,29 @@ public class LL {
     }
     // add last 
     public void addLast(String data){
-        
+        Node newNode = new Node(data);
+        if(head == null){
+            head = newNode;
+            return;
+        }
+        Node currNode = head;
+        while (currNode != null ) {
+            currNode = currNode.next;
+        }
+        currNode.next = newNode;
+    }
+    // print linklist 
+    public void print(){
+        if (head == null) {
+            System.out.println("List is empty");
+            return;
+        }
+        Node curNode = head;
+        while (curNode != null){
+            System.out.println(curNode.data + " ");
+            curNode = curNode.next;
+        }
+        System.out.println("null");
     }
 
     public static void main(String[] args) {
@@ -32,7 +54,38 @@ public class LL {
         linkLList.addFirst("a");
         linkLList.addFirst("n");
         linkLList.addFirst("o");
+        linkLList.addLast("n");
+        linkLList.addLast("t");
 
-        
     }
 }
+
+
+// public class LL {
+//     Node head;
+//     class Node{
+//         String data;
+//         Node next;
+//         Node(String data){
+//             this.data = data;
+//             this.next = next;
+//         }
+//     }
+//     // add first
+//     public void addFirst(String data){
+//         Node newNode = new Node(data);
+//         if(head == null){
+//             head = newNode;
+//             return;
+//         }
+//         newNode.next = head;
+//         head = newNode;
+//     }
+//     // add last 
+//     public void addLast(String data){
+//         Node newNode = new Node(data);
+//     }
+//     public static void main(String[] args) {
+//         LL lk = new LL();
+//     }
+// }
